aab78bd6b3d5d9f9a868279a1df6ca620f7b0733
pbfrias17 <pbfrias17@gmail.com>
2015-06-19 17:18:31 -0400

    get the ball rollin

---
+++ Unity/Ball be rollin/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//Called before physics calculations
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+	}
+}
